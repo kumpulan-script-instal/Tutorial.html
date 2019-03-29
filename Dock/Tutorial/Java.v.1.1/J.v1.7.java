5c9ee35d9aed30c2169703ea35e227d03b9ce32f
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-30 01:49:02 +0800

    Create J.v1.7.java

---
+++ Dock/Tutorial/Java.v.1.1/J.v1.7.java
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<div class="container">
+<h1>LICENSE VERSION</h1>
+</div>
+<p>licenae ini di huat agar segala tanggung jawab aktivitas yang di buat Itu menjadi TANGGUNG JAWAB ANDA okey. Jika anda sudah siap mari langsung masuk ke topik. </p>
+</div>
+<div>
+<p>CONTOH (1) membuat Model link</p>
+<div ="for html" <a href="https://travis-ci.org/on-store/Marketplace-Store"><img src="https://travis-ci.org/on-store/Marketplace-Store.svg?branch=master"></a>
+</div>
+<p> `>>>>>>>><<<<<<<<<<<<<<<<<. </p>
+<div>
+<p> BASIK; https;// travis-ci.org/on-store/Marketplace-Store'><img src="https,//travis-ci.org/on-store/Marketplace-Store.svg?branch=master" </p>
+</div>
+<div>
+<p>CONTOH (2) membuat Alamat Email Dengan Basick</p>
+</div>
+<div>
+<div id="sugestion">
+<a href="https://gmail.com">EMAIL</a>
+<div>
+<p> BASIC; "a href" adalah kata penghubung URL atau link yang kita tempelkan CONTOH: 'a href=http//Exampel
+com. jadi jika permulaan kata di huka dengan tanda {<} maka kita akhiri juga dengan tanda {>} </p>
+<div>
+</div>
+<div>
+<p> CONTOH 3 BASIK LENGKAP </p>
+</div>
+<div>
+<div url="sugestion">
+<div>
+<a href="https://github.com/kumpulan-script-instal/LICENSE">CONTOH</>
+</div>
+<div>
+<p> BERIKUR CONTOH SCRIPT <!DOCTYPE html>
+<h'tml>
+<he.ad>
+<div class='container">
+<h1.>LICENSE VERSION</h1>
+</div>
+<p>licenae ini di huat agar segala tanggung jawab aktivitas yang di buat Itu menjadi TANGGUNG JAWAB ANDA okey. Jika anda sudah siap mari langsung masuk ke topik. </p>
+</div>
+<div>
+<p>CONTOH (1) membuat Model link</p>
+<div ="for html" <a href="https;//travis-ci.org/on-store/Marketplace-Store"><img src="https://travis-ci.org/on-store/Marketplace-Store.svg?branch=master"></a>
+</div>
+<p> `>>>>>>>><<<<<<<<<<<<<<<<<. </p>
+<div>
+<p> BASIK; https;// travis-ci.org/on-store/Marketplace-Store'><img src="https,//travis-ci.org/on-store/Marketplace-Store.svg?branch=master" </p>
+</div>
+<div>
+<p>CONTOH (2) membuat Alamat Email Dengan Basick</p>
+</div>
+<div>
+<div id='sugestion">
+<a href="https;//gmail.com;>EMAIL</.a> </p>
+</div>
+<div>
+</html>
+</head>
