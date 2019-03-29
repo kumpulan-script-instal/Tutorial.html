776bbbd6c84b9a23cf4575d64c76d9dbfab4b87c
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-30 01:57:07 +0800

    Create J.v1.8.java

---
+++ Dock/Tutorial/Java.v.1.1/J.v1.8.java
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h2>:::SIGN IN::: <h2>
+<palign="center">
+<blink>contoh scrip login</h></p><form method="post" action="dblogin.php">
+<table border=0 align=center>
+<tr>
+    <td>User Name</td>
+    <td>:</td>
+    <td><input type="text" name="user"></td>
+</tr>
+<tr>
+    <td>Password</td>
+    <td>:</td>
+    <td><input type="password" name="pass"></td>
+</tr>
+</table>
+<br>
+<tableborder="0"align="center">
+<tr>
+<td>
+<input type="submit"
+value="kirim">
+</td>
+    <td>
+<input type="reset" value="reset">
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
